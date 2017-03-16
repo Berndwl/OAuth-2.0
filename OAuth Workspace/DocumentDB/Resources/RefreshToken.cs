afc6573db793a57cb750a38639df5349e36a1068
Bernd Willems <Bernd.Willems@topicus.nl>
2017-03-16 10:20:54 +0100

    DocumentDB implementatie

---
+++ OAuth Workspace/DocumentDB/Resources/RefreshToken.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DocumentDB.Resources
+{
+    public sealed class RefreshToken : Token
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        public override string Type { get; set; }
+        public override Guid ClientId { get; set; }
+        public override Guid UserId { get; set; }
+        public override string TokenString { get; set; }
+        public override List<string> Scopes { get; set; }
+
+        public RefreshToken()
+        {
+        }
+
+        public RefreshToken(AutorisatieCode code)
+        {
+            Type = GetType().Name;
+            ClientId = code.ClientId;
+            UserId = code.UserId;
+            Scopes = code.Scopes;
+            TokenString = GenerateToken(ClientId, UserId, Type);
+        }
+
+        protected override string GenerateToken(Guid client, Guid user, string type)
+        {
+            return TokenGenerate.GenerateToken(client, user, type);
+        }
+    }
+}
---
+++ OAuth Workspace/DocumentDB/Resources/TokenType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace DocumentDB.Resources
+{
+    public enum TokenType
+    {
+        [Description("Bearer token")]
+        Bearer = 1,
+        [Description("Access token")]
+        Refresh = 1,
+    }
+}
